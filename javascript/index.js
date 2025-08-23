5201c465e7881a0d9b3ae05565c46c7a5a02dd8e
sharonthy97 <sharon.thy97@gmail.com>
2025-08-23 15:54:53 +0800

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -43,7 +43,9 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )} <small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">all cities🏙️</a>
+        `;
   }
 }
 
